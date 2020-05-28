fcf67d6f98d23c573dd8c6fdfbbc9cb6f489c321
LVOVOED <LVOVOED@users.noreply.github.com>
2020-05-28 06:31:48 +0300

    Update geo.js

--- js/geo.js
+++ js/geo.js
@@ -1,22 +1,9 @@
-var results;
-var map;
-
-window.onload = function() {
-    result = document.getElementById('result');
-	
-	// Устанавливаем некоторые параметры карты. В данном примере 
-	// устанавливаются начальный уровень масштабирования и тип карты. 
-	// Информацию о других параметрах см. в документации по Google Maps.
-	var myOptions = {
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-	
-	// Создаем карту, используя установленные выше параметры
-	map = new google.maps.Map(document.getElementById("mapSurface"), myOptions);
-	
-	var marker = new google.maps.Marker({
-	position: myLatlng,
-	map: map,
-	title:"Hello World!" 
-});
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
